732d092177aa278ca49b0d926ef313e99a260bf3
HwangSlater <doorchan23@naver.com>
2025-05-13 16:50:48 +0900

    추가 사항 - 단위 테스트

--- src/main/java/com/nhnacademy/common/config/ElasticsearchConfig.java
+++ src/main/java/com/nhnacademy/common/config/ElasticsearchConfig.java
@@ -12,23 +12,36 @@ import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ElasticsearchConfig {
 
+    @Value("${elasticsearch.host}")
+    private String host;
+
+    @Value("${elasticsearch.port}")
+    private int port;
+
+    @Value("${elasticsearch.scheme}")
+    private String scheme;
+
+    @Value("${elasticsearch.username}")
+    private String username;
+
+    @Value("${elasticsearch.password}")
+    private String password;
+
     @Bean
     public ElasticsearchClient elasticsearchClient() {
-        final String username = "elastic";
-        final String password = "nhnacademy123!";
-
         BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
         credentialsProvider.setCredentials(AuthScope.ANY,
                 new UsernamePasswordCredentials(username, password));
 
         RestClientBuilder restClientBuilder = RestClient.builder(
-                new HttpHost("s4.java21.net", 9200, "http")
+                new HttpHost(host, port, scheme)
         ).setHttpClientConfigCallback(httpClientBuilder ->
                 httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider)
         );
@@ -37,8 +50,8 @@ public class ElasticsearchConfig {
         objectMapper.registerModule(new JavaTimeModule());
 
         JacksonJsonpMapper mapper = new JacksonJsonpMapper(objectMapper);
-
         ElasticsearchTransport transport = new RestClientTransport(restClientBuilder.build(), mapper);
+
         return new ElasticsearchClient(transport);
     }
 }
